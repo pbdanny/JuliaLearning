d4c3ad06e6157351dd41f4bbf2befa81c3dd9676
Danny Thanakrit <pb.danny@gmail.com>
2023-04-03 23:14:33 +0700

    update julia library

--- prob.jl
+++ prob.jl
@@ -1,1 +1,20 @@
 using Distributions
+using Makie
+using CairoMakie
+
+x = 1:10
+fig = lines(x, x.^2; label="Parabola")
+axislegend()
+fig
+
+# Assumed 17 success from total 1134
+# p of success = 17/1134 = 0.014
+
+y = 1:50
+b = Binomial(1134, 0.015)
+fig = barplot(y, pdf.(b, y); label="PMF Binomial", axis = (;title="17 success from 1134 trial"))
+axislegend()
+fig
+
+
+pdf(b, 2)
